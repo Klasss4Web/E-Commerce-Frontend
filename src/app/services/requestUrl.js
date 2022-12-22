9965173ddf9f81df5d4d2a05586870ce277cfa5b
Emmanuel Ochade <eochade16@gmail.com>
2022-12-22 07:16:05 +0000

    updated te baseurl

--- src/app/services/requestUrl.js
+++ src/app/services/requestUrl.js
@@ -1,1 +1,2 @@
-export const baseUrl = "https://my-commerce-backend.herokuapp.com";
+// export const baseUrl = "https://my-commerce-backend.herokuapp.com";
+export const baseUrl = "https://web-production-58f3.up.railway.app";
