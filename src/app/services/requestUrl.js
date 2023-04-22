0d8ea9cd971d67b2e9dfdaac21cae2ed838e3d3b
Emmanuel Ochade <eochade16@gmail.com>
2023-04-22 23:28:13 +0100

    updated the baseUrl

--- src/app/services/requestUrl.js
+++ src/app/services/requestUrl.js
@@ -1,2 +1,3 @@
 // export const baseUrl = "https://my-commerce-backend.herokuapp.com";
-export const baseUrl = "https://web-production-58f3.up.railway.app";
+// export const baseUrl = "https://web-production-58f3.up.railway.app";
+export const baseUrl = "https://ecommerce-backend-1ztr.onrender.com";
